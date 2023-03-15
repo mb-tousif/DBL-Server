df2601a20dc4383276490791ab5c57759de138e9
mb-tousif <meta.block.tousif@gmail.com>
2023-03-15 22:14:30 +0600

    Server conected with Mongodb

---
+++ utilities/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const url = process.env.MONGODB_CONNECTION_URL || "";
+mongoose.set("strictQuery", true);
+
+const Connection = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log("🗂️ DBL DB is Running");
+  } catch (error) {
+    console.log("💥 Error while connecting with DB");
+    console.log(error);
+  }
+};
+
+export default Connection;
